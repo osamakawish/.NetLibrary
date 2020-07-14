134f6854c6fb45100d7fa2b05fe0b298e8df66db
Osama Kawish <osamakawish@gmail.com>
2020-07-14 08:27:07 -0400

    Numerous RegexAtom methods added, including one to get anything contained within [] brackets. operator| defined for RegexAtom, to combine regex atoms.

---
+++ RegularExpressions/NotEscapeException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Strings
+{
+    [Serializable]
+    internal class NotEscapeException : Exception
+    {
+        public NotEscapeException()
+        {
+            Console.WriteLine("The string escapes a character that is not an escapable regex character.");
+        }
+
+        public NotEscapeException(string message) : base(message)
+        {
+        }
+
+        public NotEscapeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotEscapeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- RegularExpressions/RegexAtom.cs
+++ RegularExpressions/RegexAtom.cs
@@ -1,15 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Strings
 {
     /// <summary>
-    /// A Regex Atom corresponds to the most basic element in a regex pattern, like a . or a [abce], which isolates a single characte.
+    /// A Regex Atom corresponds to the most basic element in a regex pattern, like a . or a [abce], which isolates a single character.
+    /// Captures a function that represents what sort of characters are acceptable here.
     /// </summary>
+    /// <example>'.' corresponds to </example>
     internal class RegexAtom
     {
+        public enum Specialty { None, Character, Group };
+        public const string SpecialChars = "-,.|?*+^$[](){}\\";
+        public const string SpecialGroups = "wsd"; // For \w, \s, \d, etc. groups
+
         internal Func<char, bool> Func { get; private set; }
 
-        // An atom containing on of the letters in the string.
+        /// <summary>
+        /// Returns true if the character at index i is escaped appropriately. Also return false if inde
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public static bool IsLegallyEscaped(in string s, int i) => s[i - 1] == '\\' && Special(s[i]) == Specialty.None;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ch"></param>
+        /// <returns></returns>
+        public static Specialty Special(char ch)
+        {
+            String c = ch.ToString();
+            if (SpecialChars.Contains(c)) return Specialty.Character;
+            else if (SpecialGroups.Contains(c)) return Specialty.Group;
+            else return Specialty.None;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public RegexAtom() => Func = (ch) => false;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ch"></param>
+        public RegexAtom(char ch) => Func = (c) => c == ch;
+
+        /// <summary>
+        /// An atom containing on of the letters in the string.
+        /// </summary>
+        /// <param name="s"></param>
         public RegexAtom(string s)
         {
             // Isolate escape characters and group dashes
@@ -35,6 +79,92 @@ namespace Strings
         /// <param name="ch"></param>
         /// <returns></returns>
         public bool this[char ch] => Func(ch);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static List<RegexAtom> Atoms(string s)
+        {
+            List<RegexAtom> atoms = new List<RegexAtom>();
+            foreach (var ch in s) { atoms.Add(new RegexAtom(ch)); }
+
+            return atoms;
+        }
+
+        /// <summary>
+        /// Assumes s[i] is '-' and is within range and is not escaped.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private static RegexAtom GetRange(in string s, int i) => new RegexAtom((char)(s[i - 1] + 1), s[i + 1]);
+
+        /// <summary>
+        /// Deals with case where s[i-1] is '[' until string finishes or reaches ']'.
+        /// </summary>
+        /// <param name="s">Pattern string</param>
+        /// <param name="i">Starting index</param>
+        /// <returns></returns>
+        public static RegexAtom FromBracketed(in string s, ref int i)
+        {
+            // Note, only "\[]-" are special characters here. - only matter 
+
+            bool escaped = false;
+            RegexAtom atom = new RegexAtom();
+
+            int l = s.Length;
+
+            while ((s[i] != ']' || !escaped) && i < l)
+            {
+                char ch = s[i];
+
+                if (!escaped)
+                {
+                    if (ch == '\\') escaped = true;
+
+                    // If the '-' is between two characters not at end of string or before a bracket
+                    else if (ch == '-' && i > 0 && i + 1 < l && s[i + 1] != ']')
+                    {
+                        atom |= GetRange(s, i);
+                        i++; // Additional increment to skip next char as it's part of range.
+                    }
+
+                    else
+                    {
+                        atom |= new RegexAtom(ch);
+                    }
+                }
+                else
+                {
+                    atom |= (Special(ch)) switch
+                    {
+                        Specialty.Group => SpecialAtom(ch),
+                        _ => new RegexAtom(ch)
+                    };
+                    escaped = false;
+                }
+
+                i++;
+            }
+
+            return atom;
+        }
+
+        public static RegexAtom operator |(RegexAtom atom1, RegexAtom atom2) => new RegexAtom((ch) => atom1.Func(ch) || atom2.Func(ch));
+
+        private static RegexAtom SpecialAtom(char sp)
+        {
+            return sp switch
+            {
+                'w' => new RegexAtom(char.IsLetter),
+                's' => new RegexAtom(char.IsWhiteSpace),
+                'd' => new RegexAtom(char.IsDigit),
+                '.' => new RegexAtom((ch) => true),
+                _ => null,
+            };
+        }
     }
 
     /// <summary>
@@ -50,5 +180,7 @@ namespace Strings
         {
             RegexAtom = atom; MinCount = min; MaxCount = max;
         }
+
+        public static implicit operator AtomGroup(RegexAtom atom) => new AtomGroup(atom);
     }
 }
